d4c119dcd9d72d82516033edb5923b168db76e93
Fernando Augusto Ortolan <fernandoortolan@outlook.com>
2024-06-18 22:03:55 -0300

    Adiciona schema de mensagem

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,7 +11,27 @@ def test_root_should_return_ok_and_hello_world():
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {'message': 'Hello, World!'}  # Assert
+    assert response.json() == {'message': 'Hello, World!'}  # Assert'
+
+
+def test_should_return_ok_and_html_with_hello_world():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/hello-world-with-html')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert (
+        response.text
+        == """
+        <html>
+            <head>
+                <title>Hello, World!</title>
+            </head>
+            <body>
+                <h1>Hello, World!</h1>
+            </body>
+        </html>"""
+    )  # Assert
 
 
 client = TestClient(app)
