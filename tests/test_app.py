502c309a6d27f434ec365818a7f687cdcc191367
Fernando Augusto Ortolan <fernandoortolan@outlook.com>
2024-06-30 20:53:52 -0300

    Atualiza endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from learning_fastapi.schemas import UserPublic
+
 
 def test_root_should_return_ok_and_hello_world(client):
     response = client.get('/')
@@ -25,43 +27,67 @@ def test_create_user(client):
     }
 
 
+def test_create_user_username_already_exists_exception(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'email': 'user@example.com',
+            'password': 'mypassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_email_already_exists_exception(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'bob',
+            'email': 'test@example.com',
+            'password': 'mypassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already exists'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'sarah',
-                'email': 'sarah@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user(client, user):
     response = client.get('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'sarah',
-        'email': 'sarah@example.com',
+        'username': 'test',
+        'email': 'test@example.com',
         'id': 1,
     }
 
 
-def test_read_user_not_found_exception(client):
+def test_read_user_not_found_exception(client, user):
     response = client.get('/users/2')
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
             'username': 'sarah22',
             'email': 'sarah@example.com',
-            'password': 'banana',
+            'password': 'mynewpassword',
         },
     )
     assert response.status_code == HTTPStatus.OK
@@ -72,26 +98,26 @@ def test_update_user(client):
     }
 
 
-def test_update_user_not_found_exception(client):
+def test_update_user_not_found_exception(client, user):
     response = client.put(
         '/users/2',
         json={
             'username': 'sarah22',
             'email': 'sarah@example.com',
-            'password': 'banana',
+            'password': 'blackforest',
         },
     )
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_delete_user_not_found_exception(client):
-    response = client.delete('/users/1')
+def test_delete_user_not_found_exception(client, user):
+    response = client.delete('/users/2')
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
